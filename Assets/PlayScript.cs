250d79f5f226cec4ac7f47eeb64871654c553a6a
Dumitru Gotca <dumitru.gotca.assist@natterbox.com>
2022-06-08 22:00:08 +0300

    hide mouse and show menu on ESC

--- Assets/PlayScript.cs
+++ Assets/PlayScript.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //  if (Input.GetKey ("escape")) {
-        //          Application.Quit();
-        //         }
+         if (Input.GetKey ("escape")) {
+                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
     }
 }
